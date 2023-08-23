209767f90bd7b7204129b470bd79f694a3136758
yurii2007 <yurash862@gmail.com>
2023-08-23 18:20:24 +0300

    config package.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,6 @@
-import { Profile } from "./Profile/Profile";
-import user from '../data/user.json';
-import { Statistics } from "./Statistics/Statistics";
-import stats from '../data/data.json';
-import friends from '../data/friends.json';
-import { FriendsList } from "./FriendsList/FriendsList";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
-import transactions from '../data/transactions.json'
-
 export const App = () => {
   return (
     <div>
-    <Profile {...user}/>
-    <Statistics title="Upload stats" stats={stats} />
-    <Statistics stats={stats} />
-    <FriendsList friends={friends} />
-    <TransactionHistory items={transactions} />
     </div>
   );
 };
--- src/components/FriendsList/FriendsList.styled.js
+++
@@ -1,6 +0,0 @@
-import { styled } from 'styled-components';
-
-export const UlElem = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
