fbf7b1c6fe03e8d180f43a21658dad1caf6966a4
yurii2007 <yurash862@gmail.com>
2024-10-21 20:53:29 +0300

    init

---
+++ programs/anchor_tutorial/src/lib.rs
@@ -0,0 +1,29 @@
+use anchor_lang::prelude::*;
+
+declare_id!("GbmMA2icbWqgSJBu8GRxfJS8cWfo9xMQnVsJnpbjJBd3");
+
+#[program]
+pub mod anchor_tutorial {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        ctx.accounts.new_account.data = data;
+        msg!("Changed data: {}!", data);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub new_account: Account<'info, NewAccount>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct NewAccount {
+    data: u64,
+}
